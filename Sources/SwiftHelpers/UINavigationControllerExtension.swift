d203b721b95179f32bc3464cf28d2aa62e2e94db
Alexa Márk <alexa.mark@ingatlan.com>
2019-11-29 18:01:19 +0100

    ghj

---
+++ Sources/SwiftHelpers/UINavigationControllerExtension.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alexa Márk on 2019. 11. 29..
+//
+
+import UIKit
+
+public extension UINavigationController {
+    func popBack(_ nb: Int) {
+        let viewControllers: [UIViewController] = self.viewControllers
+        guard viewControllers.count < nb else {
+            self.popToViewController(viewControllers[viewControllers.count - nb], animated: true)
+            return
+        }
+    }
+
+    func popBack<T: UIViewController>(toControllerType: T.Type) {
+        var viewControllers: [UIViewController] = self.viewControllers
+        viewControllers = viewControllers.reversed()
+        for currentViewController in viewControllers {
+            if currentViewController .isKind(of: toControllerType) {
+                self.popToViewController(currentViewController, animated: true)
+                break
+            }
+        }
+    }}
