54c33c338888db605a90c324979ccaa54df0e1c0
Alexa Mark <alexa.mark@ingatlan.com>
2021-05-17 16:15:05 +0200

    log

--- Sources/SwiftHelpers/Extension/UIImageExtension.swift
+++ Sources/SwiftHelpers/Extension/UIImageExtension.swift
@@ -43,6 +43,33 @@ public extension UIImage {
 		return newImage!
 	}
 	
+	/// Returns a image that fills in newSize
+	 func resizedImage(newSize: CGSize) -> UIImage? {
+		 guard size != newSize else { return self }
+		 let hasAlpha = false
+		 let scale: CGFloat = 0.0
+		 UIGraphicsBeginImageContextWithOptions(newSize, !hasAlpha, scale)
+		 UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+		 draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+		 let newImage: UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+		 UIGraphicsEndImageContext()
+		 return newImage
+	 }
+
+	 /// Returns a resized image that fits in rectSize, keeping it's aspect ratio
+	 /// Note that the new image size is not rectSize, but within it.
+	 func resizedImageWithinRect(rectSize: CGSize) -> UIImage? {
+		 let widthFactor = size.width / rectSize.width
+		 let heightFactor = size.height / rectSize.height
+		 var resizeFactor = widthFactor
+		 if size.height > size.width {
+			 resizeFactor = heightFactor
+		 }
+		 let newSize = CGSize(width: size.width / resizeFactor, height: size.height / resizeFactor)
+		 let resized = resizedImage(newSize: newSize)
+		 return resized
+	 }
+	
 	func colorOverlayedImage(_ color: UIColor?) -> UIImage? {
 		guard let color = color else {
 			return nil
