40294cc1b46eebf4d6008434a532ca6f449f097d
Mark Alexa <>
2022-01-16 14:22:15 +0100

    asdfasdfasdf

--- Sources/SwiftHelpers/SwiftUI/Card.swift
+++ Sources/SwiftHelpers/SwiftUI/Card.swift
@@ -11,9 +11,11 @@ public struct Card<Content: View>: View {
     let cornerRadius: CGFloat
     let padding: CGFloat
     let backgroundColor: Color
+    let title: String?
     @ViewBuilder let content: () -> Content
 
     public init(
+        title: String? = nil,
         cornerRadius: CGFloat = .l,
         padding: CGFloat = .m,
         backgroundColor: Color = .white,
@@ -23,26 +25,43 @@ public struct Card<Content: View>: View {
         self.padding = padding
         self.content = content
         self.backgroundColor = backgroundColor
+        self.title = title
     }
 
     public var body: some View {
-        VStack(spacing: 0) {
-            content()
-                .padding(padding)
+        VStack(spacing: .s) {
+            if let title = title {
+                HStack {
+                    Text(title)
+                        .font(.headings4)
+                    Spacer()
+                }
+            }
+            VStack(spacing: 0) {
+                content()
+                    .padding(padding)
+            }
+            .background(backgroundColor)
+            .cornerRadius(cornerRadius)
         }
-        .background(backgroundColor)
-        .cornerRadius(cornerRadius)
     }
 }
 
 struct Card_Previews: PreviewProvider {
     static var previews: some View {
         ZStack {
-            Color.red.edgesIgnoringSafeArea(.all)
-            Card {
-                Text("Hello - bello")
-                    .padding()
+            Color.gray.edgesIgnoringSafeArea(.all)
+            VStack(spacing: .xxxl) {
+                Card {
+                    Text("Teszt")
+                        .frame(maxWidth: .infinity)
+                }
+                Card(title: "Title") {
+                    Text("Teszt")
+                        .frame(maxWidth: .infinity)
+                }
             }
+            .padding()
         }
     }
 }
