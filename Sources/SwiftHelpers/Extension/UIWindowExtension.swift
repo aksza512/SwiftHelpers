efaeb0a9c8c0957a41ed76613059b9e66ed5f412
Alexa Márk <alexa.mark@ingatlan.com>
2022-08-22 13:10:39 +0200

    adsfasdf

--- Sources/SwiftHelpers/Extension/UIWindowExtension.swift
+++ Sources/SwiftHelpers/Extension/UIWindowExtension.swift
@@ -36,7 +36,7 @@ public extension UIWindow {
 		}
 	}
     
-    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
             NotificationCenter.default.post(name: UIDevice.deviceDidShakeNotification, object: nil)
         }
